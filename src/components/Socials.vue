63c42c7eb15b269f26a8ac17c3e47819e3055d12
Praneeth Ravuri <prav2500@gmail.com>
2023-06-22 22:28:22 +0530

    Scroll bar styling

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="socials-div">
         <div class="logo-div">
-            <img src="../assets/images/github_bright_red.svg" alt="github logo" class="github-logo-img logo">
+            <img src="../assets/images/github_bright_red.svg" alt="github logo" class="github logo">
         </div>
         <div class="logo-div">
-            <img src="../assets/images/linkedin_bright_red.svg" alt="linkedin logo" class="linkedin-logo-img logo">
+            <img src="../assets/images/linkedin_bright_red.svg" alt="linkedin logo" class="linkedin logo">
         </div>
     </div>
 </template>
 
 <script>
+
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Socials"
